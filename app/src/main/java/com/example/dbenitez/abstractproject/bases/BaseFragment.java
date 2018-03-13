f2bc1fda30598f46dd10037b93f0f08a62fe547d
AaronBL <abautista@ia.com.mx>
2018-03-13 11:53:54 -0600

    "primer fragemnto abtract"

--- app/src/main/java/com/example/dbenitez/abstractproject/bases/BaseFragment.java
+++ app/src/main/java/com/example/dbenitez/abstractproject/bases/BaseFragment.java
@@ -8,17 +8,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.dbenitez.abstractproject.R;
 
-public class FragmentActivity extends Fragment {
+public abstract class BaseFragment extends Fragment {
 
-    SharedPreferences sharedPreferences;
+    protected String name="Aaron";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(getLayou(), container, false);
+    }
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
 
     }
+
+
+    public abstract int getLayou();
+
 }
--- app/src/main/java/com/example/dbenitez/abstractproject/fragments/MainFragment.java
+++ app/src/main/java/com/example/dbenitez/abstractproject/fragments/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.dbenitez.abstractproject.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -7,24 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dbenitez.abstractproject.R;
-import com.example.dbenitez.abstractproject.bases.FragmentActivity;
+import com.example.dbenitez.abstractproject.bases.BaseFragment;
 
 /**
  * Created by abautista on 3/13/2018.
  */
 
-public class MainFragment extends FragmentActivity {
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-     super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_main,container,false);
-        return view;
+public class MainFragment extends BaseFragment {
 
-    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        String name2 = name;
+    }
+
+    @Override
+    public int getLayou() {
+        return R.layout.fragment_main;
     }
 }
